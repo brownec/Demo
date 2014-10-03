96023c372f594036dd64d2725cef168df95bb143
brownec <cbrowne77@gmail.com>
2014-10-03 15:44:07 +0100

    This is the file for CPUs scrpt.sh

--- week3/script.sh
+++ week3/script.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+#: Title : script.sh
+#: Date : 3 October 2014
+#: Author : Cliff Browne
+#: Version : 1.1
+#: Description : counts the number of CPUs in a VM 
+#: Options : no options
+
 # this is a script
 # this is a comment
 
-grep processor /proc/cpuinfo | wc -l
+cpucnt=$( grep processor /proc/cpuinfo | wc -l )
+
+printf "The number of CPUs is: %s\n" $cpucnt
+
+
+
