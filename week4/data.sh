ba89633dbb9f81c26c58be5e8111232ae50a4def
brownec <cbrowne77@gmail.com>
2014-10-10 15:44:16 +0100

    Q5 User Information added to data.sh

--- week4/data.sh
+++ week4/data.sh
@@ -7,9 +7,23 @@
 #: Options : none
 
 #Create a new script called scripts/data.sh, collect from the command line:
+printf "User Information"
 #- the users name
+printf "Please enter in your name and press [ENTER]:"
+read name
 #- the month they we born in
+printf "Please enter the month you where born:"
+read month
 #- the favourite color
+printf "Please enter your favourite colour:"
+read color
+
+
 #and print to the user the information the form
 #Hello ... you were born in ... and your favourite color is ...
+printf "Hello $name , you were born in $month and your favourite colour is $color"
+
 #Using git from the command line, add, commit and push this script to the git repo
+
+
+
