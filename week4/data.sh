c1b9436c622759e489894a16be64475ba7cf20b9
brownec <cbrowne77@gmail.com>
2014-10-10 15:28:40 +0100

    Data file for Q5

---
+++ week4/data.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#: Title : EAA Week 4 Lab3(b)
+#: Date : 10 October 2014
+#: Author : Cliff Browne
+#: Version : 1.1
+#: Description : More Scripting 
+#: Options : none
+
+#Create a new script called scripts/data.sh, collect from the command line:
+#- the users name
+#- the month they we born in
+#- the favourite color
+#and print to the user the information the form
+#Hello ... you were born in ... and your favourite color is ...
+#Using git from the command line, add, commit and push this script to the git repo
