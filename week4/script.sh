5a668edb289a36e0701ca28368fa789d317bd562
brownec <cbrowne77@gmail.com>
2014-10-10 15:26:28 +0100

    Q5

--- week4/script.sh
+++ week4/script.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-#: Title : Script.sh file
+#: Title : Week 4, Lab 3(b) More Scripting
 #: Date : 10 October 2014
 #: Author : Cliff Browne
 #: Version : week4_1.1
 #: Description : Test script
 #: Options : 
+
+
 
